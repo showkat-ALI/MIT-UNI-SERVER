cf9dfb3048bf2b7a550d439b0a0cae220c490f1b
Showkat  Ali <showkat.ali2634@gmail.com>
2025-01-12 17:02:17 +0600

    unhandledRejection and unCaughtException handling

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import { Server } from 'http';
 
+let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
   } catch (err) {
@@ -16,3 +18,17 @@ async function main() {
 }
 
 main();
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
