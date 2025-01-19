ce6da96de33aa10139d533d75d298393cb491a02
Showkat  Ali <showkat.ali2634@gmail.com>
2025-01-19 21:19:44 +0600

    auth-creating

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,7 +4,11 @@ import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
-import { generatedFacultyID, generateStudentId } from './utils/user.utils';
+import {
+  generateAdminId,
+  generatedFacultyID,
+  generateStudentId,
+} from './utils/user.utils';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import mongoose from 'mongoose';
 import AppError from '../../error/AppError';
@@ -13,6 +17,8 @@ import { AcademicDepartment } from '../academicDepartment/academicDepartment.mod
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { TFaculty } from '../faculty/faculty.interface';
 import { Faculty } from '../faculty/faculty.model';
+import { TAdmin } from '../admin/admin.interface';
+import { Admin } from '../admin/admin.model';
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   // create a user object
@@ -99,8 +105,54 @@ const createFacultyIntoDB = async (password: string, payLoad: TFaculty) => {
     throw new Error(err);
   }
 };
+const createAdminIntoDB = async (password: string, payload: TAdmin) => {
+  // create a user object
+  const userData: Partial<TUser> = {};
+
+  //if password is not given , use deafult password
+  userData.password = password || (config.default_password as string);
+
+  //set student role
+  userData.role = 'admin';
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    //set  generated id
+    userData.id = await generateAdminId();
+
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session });
+
+    //create a admin
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create admin');
+    }
+    // set id , _id as user
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; //reference _id
+
+    // create a admin (transaction-2)
+    const newAdmin = await Admin.create([payload], { session });
+
+    if (!newAdmin.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create admin');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return newAdmin;
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
+  }
+};
 
 export const UserServices = {
   createStudentIntoDB,
   createFacultyIntoDB,
+  createAdminIntoDB,
 };
--- src/app/modules/user/utils/user.utils.ts
+++ src/app/modules/user/utils/user.utils.ts
@@ -43,6 +43,23 @@ export const generateStudentId = async (payload: TAcademicSemester) => {
 
   return incrementId;
 };
+export const findLastAdminId = async () => {
+  const lastAdmin = await User.findOne(
+    {
+      role: 'admin',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
+};
 export const findLastFacultyID = async () => {
   const lastFaculty = await User.findOne(
     {
@@ -67,5 +84,18 @@ export const generatedFacultyID = async () => {
   }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `F-${incrementId}`;
+  return incrementId;
+};
+export const generateAdminId = async () => {
+  let currentId = (0).toString();
+  const lastAdminId = await findLastAdminId();
+
+  if (lastAdminId) {
+    currentId = lastAdminId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `A-${incrementId}`;
   return incrementId;
 };
